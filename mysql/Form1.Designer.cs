d45166deba814913c479cfe7b8c08c6f7c3e3527
zeroqs <smthq@bk.ru>
2024-01-19 01:27:55 +0300

    add comments

--- mysql/Form1.Designer.cs
+++ mysql/Form1.Designer.cs
@@ -85,7 +85,6 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(306, 305);
             this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
--- mysql/Form1.cs
+++ mysql/Form1.cs
@@ -16,66 +16,61 @@ namespace mysql
 {
     public partial class Form1 : Form
     {
-        string sql;
-        MySqlDataAdapter da;
+        string sql;    //объясвление запросов
+        MySqlDataAdapter da;     //методы библиотеки, позволяющие подключится к MySql
         DataTable ds;
         MySqlConnection conn;
 
-        public Form1()
+        public Form1()    //создание 1 формы
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)    //метод, который отрабатывает во время загрузки формы
         {
-            conn = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=hotel");
-            try
+            conn = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=hotel");   //Sql соединение, подключение к серверу, параметры
+            try     //конструкция обработки исключений (ошибок)                                                    //подключения предоставляет программа
             {
-                conn.Open();
-                MessageBox.Show("Подключение к БД установлено");
+                conn.Open();           //попытка открытия соединения с базой данных
+                MessageBox.Show("Подключение к БД установлено");        //если все хорошо выводится
             }
-            catch (Exception ex)
+            catch (Exception ex)      //если ошибка в подключении к базе данных то уходим в catch
             {
-                MessageBox.Show("Проблемы с подключением к БД \n\r" + ex.ToString());
+                MessageBox.Show("Проблемы с подключением к БД \n\r" + ex.ToString());   //вывод сообщения + ошибки
             }
-            GetList1();
+            GetList1();    //вызов функции
         }
 
-        private void GetList1()
+        private void GetList1()    //объявление функции      //private - модификатор, который обозначает, что данная функция может использоваться только внутри класса
         {
-            sql = "show tables";
+            sql = "show tables";    //вывод всех таблиц на экран
 
-            da = new MySqlDataAdapter(sql, conn);
-            ds = new DataTable();
-            da.Fill(ds);
-            dataGridView1.DataSource = ds;
+            da = new MySqlDataAdapter(sql, conn);   //Создается объект MySqlDataAdapter с использованием SQL-запроса и соединения с базой данных
+            ds = new DataTable();   //Создается объект DataTable, который будет хранить результаты запроса в виде таблицы в памяти
+            da.Fill(ds);   //Заполняет DataTable данными из базы данных с помощью адаптера данных
+            dataGridView1.DataSource = ds;   //Устанавливает источник данных для DataGridView равным заполненной таблице ds,
+        }                                    // что приводит к отображению данных в таблице на форме
+
+        private void ClearOutput()     // метод, который очищает вывод на форме
+        {
+            ArrayList Empty = new ArrayList();  //Создается объект ArrayList с именем Empty
+                             //создается пустой список для использования в dataGridView1.DataSource для того, чтобы очистить содержимое
+
+            textBox1.Clear();   //Очищает содержимое текстового поля textBox1
+            dataGridView1.DataSource = Empty;    //Устанавливает источник данных для DataGridView равным пустому списку, что приводит к удалению данных из таблицы на форме
+            dataGridView1.Refresh();   //Обновляет отображение DataGridView
         }
 
-        private void ClearOutput()
+        private void button1_Click(object sender, EventArgs e)   //метод, который выполняется при нажатии на кнопку с именем button1
         {
-            ArrayList Empty = new ArrayList();
-
-
-            textBox1.Clear();
-            dataGridView1.DataSource = Empty;
-            dataGridView1.Refresh();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ClearOutput();
+            ClearOutput();   //Вызывает метод ClearOutput(), который очищает вывод на форме
             sql = "SELECT Nomer, \r\nCOUNT(*) AS max_number_settlements\r\nFROM visit\r\nGROUP BY Nomer\r\nHAVING COUNT(*) = (\r\n    SELECT MAX(cnt)\r\n    FROM (\r\n        SELECT COUNT(*) AS cnt\r\n        FROM visit\r\n        GROUP BY Nomer\r\n    ) AS subquery\r\n);";
 
-            da = new MySqlDataAdapter(sql, conn);
+            da = new MySqlDataAdapter(sql, conn);  
             ds = new DataTable();
-            textBox1.Text = sql;
-            da.Fill(ds);
+            textBox1.Text = sql;    //Устанавливает текст в textBox1 равным строке sql для отображения самого SQL-запроса на форме
+            da.Fill(ds);     //Заполняет DataTable данными из базы данных с помощью адаптера данных
             dataGridView1.DataSource = ds;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click_1(object sender, EventArgs e)
@@ -101,5 +96,6 @@ namespace mysql
             da.Fill(ds);
             dataGridView1.DataSource = ds;
         }
+
     }
 }
